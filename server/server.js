0dba2a067a0f7bbae0dccc5f7fa6584cd8a18df2
JuanHDZ <juand_10193@hotmail.com>
2018-06-06 12:53:01 -0500

    Servidor rest de usuario

--- server/server.js
+++ server/server.js
@@ -1,6 +1,7 @@
 require('./config/config')
 
 const express = require('express')
+const mongoose = require('mongoose')
 const app = express()
 const bodyParser = require('body-parser')
 
@@ -10,42 +11,19 @@ app.use(bodyParser.urlencoded({ extended: false }))
 // parse application/json
 app.use(bodyParser.json())
 
+//requerir rutas
+app.use(require('./routes/usuario'))
 
 
 
-app.get('/usuario', (req, res) => {
-    res.json('Hola mundo')
-})
 
-app.post('/usuario', (req, res) => {
-    let body = req.body
+mongoose.connect(process.env.URLDB, (err, res) => {
+    if (err) throw err
 
-    if (body.nombre === undefined) {
-        res.status(400).json({
-            ok: false,
-            mensaje: 'El nombre es necesario'
-        })
-
-    } else {
-
-        res.json({
-            persona: body
-        })
-    }
+    console.log('Base de datos Online')
 
 })
 
-app.put('/usuario/:id', (req, res) => {
-    let id = req.params.id
-
-    res.json({
-        id
-    })
-})
-
-app.delete('/usuario', (req, res) => {
-    res.json('Delete usuario')
-})
 
 app.listen(process.env.PORT, () => {
     console.log(`Servidor corriendo en el puerto ${process.env.PORT}`)
