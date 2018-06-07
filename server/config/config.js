18417a555c0b00b84a7ae339a2df90fff02a5e12
JuanHDZ <juand_10193@hotmail.com>
2018-06-07 02:02:36 -0500

    Google-sig-In agregado

--- server/config/config.js
+++ server/config/config.js
@@ -38,3 +38,9 @@ if (process.env.NODE_ENV === 'dev') {
 }
 
 process.env.URLDB = urlDB
+
+///===================
+///Google Client ID
+///===================
+
+process.env.CLIENT_ID = process.env.CLIENT_ID || "654349897956-u6rl85jgaedv1jaa3on6tqept6gvha5i.apps.googleusercontent.com"
